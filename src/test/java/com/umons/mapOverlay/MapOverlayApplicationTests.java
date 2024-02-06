cb28663fc13834aab0ec8d434c494eae7fad1950
3npC0nf1g <teumayouomo113@gmail.com>
2024-02-06 14:35:06 +0100

    projet: mise en place du projet par défaut

---
+++ src/test/java/com/umons/mapOverlay/MapOverlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umons.mapOverlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapOverlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
