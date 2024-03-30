48ddfff071e00d78751d023874423f9fb80a04e0
3npC0nf1g <teumayouomo113@gmail.com>
2024-03-30 09:09:20 +0100

    feat: affichage des droites sur depuis un fichier txt

--- src/main/java/com/mapoverlay/HelloController.java
+++ src/main/java/com/mapoverlay/HelloController.java
@@ -3,10 +3,22 @@ package com.mapoverlay;
 import com.mapoverlay.model.MapOverlay;
 import com.mapoverlay.model.data.Point;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HelloController {
+
     @FXML
-    private Label welcomeText;
+    private Canvas canvas;
 
     MapOverlay MO = new MapOverlay();
 
@@ -17,7 +29,39 @@ public class HelloController {
 
     @FXML
     protected void onImportButtonClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choisir un fichier");
 
+        // Définir un filtre pour ne montrer que les fichiers avec l'extension .txt
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Fichiers texte (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Afficher la boîte de dialogue de sélection de fichier
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
+                GraphicsContext gc = canvas.getGraphicsContext2D();
+                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+                double canvasWidth = canvas.getWidth();
+                double canvasHeight = canvas.getHeight();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] coords = line.trim().split("\\s+");
+                    if (coords.length == 4) {
+                        double x1 = Double.parseDouble(coords[0]) * 3 ;
+                        double y1 = Double.parseDouble(coords[1]) * 3;
+                        double x2 = Double.parseDouble(coords[2]) * 3;
+                        double y2 = Double.parseDouble(coords[3])* 3 ;
+                        gc.setStroke(Color.BLACK);
+                        gc.strokeLine(x1, y1, x2, y2);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
 }
