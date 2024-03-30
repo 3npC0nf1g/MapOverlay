4ee141ccecc4ab67dc87eadaad9e1763e400315d
3npC0nf1g <teumayouomo113@gmail.com>
2024-03-30 18:30:50 +0100

    feat: affichage superposé de plusieurs droites

--- src/main/java/com/mapoverlay/HelloController.java
+++ src/main/java/com/mapoverlay/HelloController.java
@@ -1,6 +1,7 @@
 package com.mapoverlay;
 
 import com.mapoverlay.model.MapOverlay;
+import com.mapoverlay.model.data.Segment;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -10,11 +11,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
 
+
     @FXML
     private Canvas canvas;
+
+    @FXML
+    private Canvas mapOverlayCanvas;
 
     MapOverlay MO = new MapOverlay();
 
@@ -26,35 +33,40 @@ public class HelloController {
     @FXML
     protected void onImportButtonClick() {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Choisir un fichier");
+        fileChooser.setTitle("Choisir des fichiers de segments de droite");
 
         // Définir un filtre pour ne montrer que les fichiers avec l'extension .txt
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Fichiers texte (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extFilter);
 
-        // Afficher la boîte de dialogue de sélection de fichier
-        File selectedFile = fileChooser.showOpenDialog(null);
+        // Afficher la boîte de dialogue de sélection de fichiers
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(null);
 
-        if (selectedFile != null) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
-                GraphicsContext gc = canvas.getGraphicsContext2D();
-                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        if (selectedFiles != null) {
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] coords = line.trim().split("\\s+");
-                    if (coords.length == 4) {
-                        double x1 = Double.parseDouble(coords[0]) * 3 ;
-                        double y1 = Double.parseDouble(coords[1]) * 3;
-                        double x2 = Double.parseDouble(coords[2]) * 3;
-                        double y2 = Double.parseDouble(coords[3])* 3 ;
-                        gc.setStroke(Color.BLACK);
-                        gc.strokeLine(x1, y1, x2, y2);
+            // Parcourir la liste des fichiers sélectionnés
+            for (File selectedFile : selectedFiles) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        String[] coords = line.trim().split("\\s+");
+                        if (coords.length == 4) {
+                            double x1 = Double.parseDouble(coords[0]) * 2 ;
+                            double y1 = Double.parseDouble(coords[1]) * 2;
+                            double x2 = Double.parseDouble(coords[2]) * 2;
+                            double y2 = Double.parseDouble(coords[3]) * 2;
+
+                            gc.setStroke(Color.BLACK);
+                            gc.strokeLine(x1, y1, x2, y2);
+                        }
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
+
 }
